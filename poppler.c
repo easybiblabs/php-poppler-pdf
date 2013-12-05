5903f895961ea22cf086c56d4483397fe6b0b043
Richard Wossal <richard@r-wos.org>
2013-12-05 14:10:28 +0100

    now returning a hash map with pdf info

--- poppler.c
+++ poppler.c
@@ -41,6 +41,15 @@ ZEND_GET_MODULE(poppler)
 
 /* main */
 
+void add_assoc_maybe_string(zval *ret, const char *key, char *s)
+{
+    if (s == NULL) {
+        add_assoc_null(ret, key);
+    } else {
+        add_assoc_string(ret, key, s, 1);
+    }
+}
+
 PHP_FUNCTION(poppler_pdf_info)
 {
     char *name;
@@ -48,6 +57,7 @@ PHP_FUNCTION(poppler_pdf_info)
 
     PopplerDocument *doc;
     GError *err = NULL;
+    gchar *s = NULL;
 
     if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &name, &name_len) == FAILURE) {
         RETURN_NULL();
@@ -60,12 +70,19 @@ PHP_FUNCTION(poppler_pdf_info)
         RETURN_NULL();
     }
 
-    php_printf("title: %s\n", poppler_document_get_title(doc));
-    php_printf("author: %s\n", poppler_document_get_author(doc));
-    php_printf("subject: %s\n", poppler_document_get_subject(doc));
-    php_printf("keywords: %s\n", poppler_document_get_keywords(doc));
-    php_printf("creator: %s\n", poppler_document_get_creator(doc));
+    array_init(return_value);
+
+    add_assoc_maybe_string(return_value, "pdf_version", poppler_document_get_pdf_version_string(doc));
+
+    add_assoc_maybe_string(return_value, "title", poppler_document_get_title(doc));
+    add_assoc_maybe_string(return_value, "author", poppler_document_get_author(doc));
+    add_assoc_maybe_string(return_value, "subject", poppler_document_get_subject(doc));
+    add_assoc_maybe_string(return_value, "keywords", poppler_document_get_keywords(doc));
+    add_assoc_maybe_string(return_value, "creator", poppler_document_get_creator(doc));
+    add_assoc_maybe_string(return_value, "producer", poppler_document_get_producer(doc));
+
+    //add_assoc_string(return_value, "producer", poppler_document_get_creation_date(doc), 1);
+    //add_assoc_string(return_value, "producer", poppler_document_get_modification_date(doc), 1);
 
     free(doc);
-    RETURN_STRING("Hello World", 1);
 }
