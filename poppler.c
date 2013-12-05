a441535f3758883bb8a21e903b48d9bd3a1f7b20
Richard Wossal <richard@r-wos.org>
2013-12-05 17:46:51 +0100

    added text extraction

--- poppler.c
+++ poppler.c
@@ -129,15 +129,46 @@ PHP_FUNCTION(poppler_pdf_info)
 PHP_FUNCTION(poppler_pdf_text)
 {
     PopplerDocument *doc;
-    long page;
+    PopplerPage *page;
+    long page_i;
+    char *text;
+    size_t textlen;
+    GList *attr_list;
     zval *zdoc;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &zdoc, &page) == FAILURE) {
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &zdoc, &page_i) == FAILURE) {
         RETURN_NULL();
     }
     ZEND_FETCH_RESOURCE(doc, PopplerDocument*, &zdoc, -1, PHP_POPPLER_DOCUMENT_NAME, le_poppler_document);
 
-    /// XXX TODO: get page; get text + attributes + merge them; return resulting data;
+    page = poppler_document_get_page(doc, page_i);
+    if (page == NULL) {
+        RETURN_NULL();
+    }
+
+    text = poppler_page_get_text(page);
+    textlen = strlen(text);
+
     array_init(return_value);
-    add_assoc_long(return_value, "page", page);
+
+    attr_list = poppler_page_get_text_attributes(page);
+    {
+        GList *el;
+        PopplerTextAttributes *attr;
+        zval *text_part;
+
+        for (el = g_list_first(attr_list); el; el = el->next) {
+            attr = el->data;
+            ALLOC_INIT_ZVAL(text_part);
+            array_init(text_part);
+
+            add_assoc_stringl(text_part, "text", text + (attr->start_index), (attr->end_index) - (attr->start_index), 1);
+            add_assoc_maybe_string(text_part, "font_name", attr->font_name);
+            add_assoc_long(text_part, "font_size", attr->font_size);
+            add_assoc_bool(text_part, "is_underlined", attr->is_underlined);
+            add_next_index_zval(return_value, text_part);
+        }
+    }
+
+    poppler_page_free_text_attributes(attr_list);
 }
